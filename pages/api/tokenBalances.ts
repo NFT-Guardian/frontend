b3d845bc7abdcbb1efe38b5d3c1ff9e3869508ea
Bertrand <bertrandrigal.pro@gmail.com>
2023-08-12 16:12:19 +0200

    clean up the queries

--- pages/api/tokenBalances.ts
+++ pages/api/tokenBalances.ts
@@ -1,11 +1,51 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import TokenList from '../../utils/tokenList';
 import ChainList from '../../utils/chainList';
+import { log } from 'console';
 
 const key = {
     etherscan: process.env.ETHERSCAN_KEY,
-    optimism:  '0000',
-    base:  '0000',
+    optimism: '0000',
+    base: '0000',
+}
+
+async function getSingleTokenBalance(chain: string, address: string, tokenAddress: string) {
+    switch (chain) {
+        case 'ethereum':
+            const balance = await getSingleTokenBalanceEth(chain, address, tokenAddress);
+            console.log('balanceeeeeee : ', balance);
+            
+            return balance;
+        // case 'optimism':
+        //     return getSingleTokenBalanceOp(chain, address, tokenAddress);
+        // case 'base':
+        //     return getSingleTokenBalanceBase(chain, address, tokenAddress);
+        default:
+            throw new Error('Invalid chain');
+    }
+}
+
+async function getSingleTokenBalanceEth(chain: string, address: string, tokenAddress: string) {
+    const url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${tokenAddress}&address=${address}&tag=latest&apikey=${key.etherscan}`;
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        console.log(data);
+        if (data.status === '1') {
+            return data.result;
+        } else {
+            throw new Error('Error fetching token balances');
+        }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error fetching token balances');
+    }
+}
+function getSingleTokenBalanceOp(chain: string, address: string, tokenAddress: string) {
+    return null;
+}
+function getSingleTokenBalanceBase(chain: string, address: string, tokenAddress: string) {
+    return null;
 }
 
 // example http://localhost:3000/api/tokenBalances?chain=ethereum&address=0x57d90b64a1a57749b0f932f1a3395792e12e7055&tokenAddress=0xe04f27eb70e025b78871a2ad7eabe85e61212761
@@ -30,33 +70,15 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         res.status(400).json({ error: "Token not supported" });
     }
 
-    let url = null;
-    switch (chain) {
-        case 'optimism':
-            break;
-        case 'ethereum':
-            url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${tokenAddress}&address=${address}&tag=latest&apikey=${key.etherscan}`;
-        default:
-            break;
-    }
-
-    if (url === null) {
-        return res.status(400).json({ error: "Cannot build url" })
-    }
-    
-    try {
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                if (data.status === '1') {
-                    res.status(200).json(data.result);
-                } else {
-                    return res.status(400).json({ error: "Error fetching token balances" })
-                }
-            });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json(error);
-    }
+    // For loop on each tokens
+    getSingleTokenBalance(chain, address, tokenAddress)
+        .then((balance) => {
+            console.log('yeaaa', balance)
+            res.status(200).json(balance);
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(400).json({ error: "Error fetching token balances" })
+        })
+    // endfor
 }
